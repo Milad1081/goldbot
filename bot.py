94339213b9e7017371fa5602948b7318f3592a8b
Milad1081 <m.talebi108@gmail.com>
2025-09-28 00:13:23 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
-import os
-import requests
+import os, requests
 
 token = os.environ["BOT_TOKEN"]
 chat_id = os.environ["CHANNEL_ID"]
 
-text = "سلام از GitHub Actions 🚀"
-
 url = f"https://api.telegram.org/bot{token}/sendMessage"
-r = requests.post(url, data={"chat_id": chat_id, "text": text})
+r = requests.post(url, data={"chat_id": chat_id, "text": "سلام از GitHub Actions 🚀"})
 
 print("Status:", r.status_code)
 print("Response:", r.text)
